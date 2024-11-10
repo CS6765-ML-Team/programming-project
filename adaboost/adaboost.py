9f427356ca7d4b9a49f3274f1bf01080c7df903d
EthanG78 <ethangarnier78@gmail.com>
2024-11-10 14:25:47 -0400

    Implemented Entropy calculations for data sets. Fixed bug with feature/targets/

--- adaboost/adaboost.py
+++ adaboost/adaboost.py
@@ -8,8 +8,8 @@ from ucimlrepo import fetch_ucirepo   # Letter recognition data set
 letter_recognition = fetch_ucirepo(id=59) 
 
 # Store the features and targets
-features = letter_recognition.data
-targets = letter_recognition.data
+features = letter_recognition.data.features
+targets = letter_recognition.data.targets
 
 # Store a combination of features and targets
 total = pd.DataFrame(data=letter_recognition.data.original, columns=letter_recognition.data.headers)
@@ -26,3 +26,20 @@ for label in targets.columns.values:
 
 # Split the data into training and testing sets (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.2, random_state=42)
+
+def calc_entropy(S, target_name):
+  """ Calculate the entropy for a sample
+  of training examples based on Shanon's 
+  Entropy Model. 
+  """
+  entropy = 0
+  total_count = S[target_name].count()
+  for frequency in S[target_name].value_counts():
+    probability = frequency / total_count
+    entropy += -1 * probability * np.log2(probability)
+
+  return entropy
+
+
+def calc_info_gain(S, A):
+  pass
